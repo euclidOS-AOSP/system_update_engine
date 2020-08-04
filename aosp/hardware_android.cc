c5c871fc0f4d6990f0a9aa8b50c2172b66fcbc75
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-08-04 01:48:32 -0300

    update_engine: Always disable signature and downgrade checks
    Change-Id: Ibd9ca7d69f3824d5302c7c76c0baf4b9d41fced6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- aosp/hardware_android.cc
+++ aosp/hardware_android.cc
@@ -255,8 +255,7 @@ int64_t HardwareAndroid::GetBuildTimestamp() const {
 // Returns true if the device runs an userdebug build, and explicitly allows OTA
 // downgrade.
 bool HardwareAndroid::AllowDowngrade() const {
-  return GetBoolProperty("ro.ota.allow_downgrade", false) &&
-         GetProperty("ro.build.type", "") != "user";
+  return true;
 }
 
 bool HardwareAndroid::GetFirstActiveOmahaPingSent() const {
